01078b425c43ddbf16024c03a57f431173a9fa0e
petertran98 <Petertran198@gmail.com>
2021-05-06 21:41:14 -0500

    Added bootstrap

---
+++ src/Components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+export default function App() {
+    return (
+        <div className='jumbotron'>
+            <Button variant='primary'>Primary</Button>
+        </div>
+    );
+}
